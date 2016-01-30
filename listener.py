17c69b6a60865ec591b403257538d71e017d8e47
kmonte <lazykylem@gmail.com>
2016-01-30 12:44:34 -0500

    added support for pitch scrolling

--- listener.py
+++ listener.py
@@ -15,6 +15,9 @@ class ActionListener(Leap.Listener):
     AVG = 10
     count = 0
     action = None
+    use_vertscroll = true
+    roll_offset = math.pi/2 - .8
+    vert_scroll_limit = pi/8
     
     def on_init(self, controller):
         controller.set_policy_flags(Leap.Controller.POLICY_BACKGROUND_FRAMES)
@@ -45,6 +48,18 @@ class ActionListener(Leap.Listener):
                 if (not indexFingerList.is_empty and indexFinger.direction.z < 0):
                     average += indexFinger.tip_position
                     valid_fingers += 1
+                if(self.use_vertscroll): 
+                    if (prev_finger.is_valid and finger.is_valid):
+                        diff = prev_finger.tip_position.y - finger.tip_position.y
+                        print diff
+       
+                        if ( math.fabs(diff) > self.SCROLL_THRESHOLD ):
+                            self.action.scroll(math.copysign(1,-diff))
+                elif finger.is_valid and finger.type == Leap.Finger.TYPE_INDEX:
+                    roll = finger.direction.roll + self.roll_offset
+                    if math.fabs(roll) > self.vert_scroll_limit:
+                        self.action.scroll(self.vert_scroll_limit-roll)
+                        print roll
                   
             average /= valid_fingers
             if not valid_fingers == 0:              
