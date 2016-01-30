4e1243393b8400cb4eddfcf3eb15ed81f56ed2ba
erabaum <erabaum@gmail.com>
2016-01-30 02:03:49 -0500

    Added self declarations to globals

--- listener.py
+++ listener.py
@@ -1,6 +1,6 @@
 import os, sys, inspect, thread, time
 src_dir = os.path.dirname(inspect.getfile(inspect.currentframe()))
-arch_dir = '../lib/x64' if sys.maxsize > 2**32 else '../lib/x86'
+arch_dir = '../lib/x64' if sys.maxsize > 2 ** 32 else '../lib/x86'
 sys.path.insert(0, os.path.abspath(os.path.join(src_dir, arch_dir)))
 
 import Leap
@@ -20,20 +20,20 @@ class ActionListener(Leap.Listener):
         print "Connected" 
 
     def on_frame(self, controller):
-        if (count == AVG):
+        if (self.count == self.AVG):
             valid_fingers = 0
             average = Leap.Vector()
 
-            for i in range(0,AVG-1):
+            for i in range(0, self.AVG - 1):
                 finger = controller.frame(i).fingers.frontmost
                 if (finger.is_valid):
                     average += finger.tip_position
-                    valid_fingers++
+                    valid_fingers += 1
                  
             average /= valid_fingers
             count = 0
-            action.check(average)
+            # self.action.check(average)
                     
         else:
-            count++
+            self.count += 1
 
