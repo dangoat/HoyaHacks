d69c1f6425e7cb906c95c3eb6640a1785b2c1d6d
kmonte <lazykylem@gmail.com>
2016-01-31 00:15:12 -0500

    for pinch calcs

---
+++ main.h
@@ -0,0 +1,13 @@
+//
+//  main.h
+//  
+//
+//  Created by Kyle Montemayor on 1/30/16.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface main : NSObject
+
+@end
---
+++ main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  
+//
+//  Created by Kyle Montemayor on 1/30/16.
+//
+//
+
+#import "main.h"
+
+@implementation main
+
+@end
